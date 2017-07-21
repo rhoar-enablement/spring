8beb79c92c39e663abd01a295f0f04802e7ed910
tqvarnst <tqvarnst@redhat.com>
2017-07-21 08:35:42 +0200

    Added solution for lab2

---
+++ lab2/src/main/java/com/redhat/coolstore/ProductCatalogRESTService.java
@@ -0,0 +1,39 @@
+package com.redhat.coolstore;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/products")
+public class ProductCatalogRESTService {
+	
+	@Value("${catalog.default.products}")
+	String defaultProducts;
+	
+	@Autowired
+	ProductRepository catalog;
+	
+	@GET
+	public Response list() {
+		List<Product> products = catalog.findAll();
+		if(products==null || products.isEmpty()) {
+			return Response.serverError().entity("Did not found any products").build();
+		}
+		return Response.ok(products,MediaType.APPLICATION_JSON).build();
+	}
+	
+	@GET
+	@Path("/defaultlist")
+	public Response defaultList() {
+		return Response.ok(defaultProducts.split(","),MediaType.APPLICATION_JSON).build();
+	}
+	
+}
---
+++ lab2/src/test/java/com/redhat/coolstore/ProductCatalogApplicationDevTests.java
@@ -0,0 +1,60 @@
+package com.redhat.coolstore;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("dev")
+@DataJpaTest
+public class ProductCatalogApplicationDevTests {
+	
+    @Autowired
+    private ProductRepository products;
+	
+	@Test
+	public void testFindAll() {
+		List<Product> productList = products.findAll();
+		assertEquals(productList.size(), 8);
+	}
+	
+	@Test
+	public void testFindOne() {
+		Product product = products.findOne(444435L);
+		assertTrue("Oculus Rift".equals(product.getName()));
+	}
+	
+	
+	@Test
+	public void testSaveAndDeleteProduct() {
+		
+		Product newProduct = new Product();
+		newProduct.setName("Test Prod");
+		newProduct.setDescription("This is a description");
+		newProduct.setPrice(10.00d);
+		
+		Product product = products.save(newProduct);
+		long id = product.getItemId();
+		
+		assertNotNull(products.findOne(id));
+		
+		products.delete(product);
+		
+		assertNull(products.findOne(id));
+	}
+	
+	
+
+}
