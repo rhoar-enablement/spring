4671a41d2a8afbd27c762c09d50232e27bba0f37
Thomas Qvarnström <tqvarnst@Q2.lan>
2017-08-10 22:31:27 +0200

    Updated lab2 init to reflect the changes to lab1

---
+++ lab2/src/main/java/com/redhat/coolstore/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.coolstore.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+}
